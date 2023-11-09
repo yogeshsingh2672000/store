8fc87904152336ce993ee7300f1fdfc858848648
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-11-09 12:45:26 +0530

    layout in progress

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="bg-yellow-300 text-black text-3xl">Hello World</div>
+    <div className="App bg-yellow-300 text-black text-3xl h-screen">
+      <header>header</header>
+      <main>main</main>
+      <footer>footer</footer>
     </div>
   );
 }
